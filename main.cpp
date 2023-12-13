6f26e40f24d1d014c5799b87b2d6661d6f4eed8e
Yaroslav <yaroslav.bojko.82@bk.ru>
2023-12-13 14:13:55 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int human;
+    std::cout << "How many men live in the city? ";
+    std::cin >> human;
+
+    int barberHour = 1;
+    int changeBarber = 8;
+    int barber;
+
+    std::cout << "How many barbers are already working? ";
+    std::cin >> barber;
+    std::cout << std::endl;
+
+    int necessaryBarber;
+    int barberMonth = barberHour * changeBarber * 30;
+    necessaryBarber = human / barberMonth;
+    if (human % barberMonth == 0) {
+        necessaryBarber = necessaryBarber;
+    } else {
+        necessaryBarber += 1;
+    }
+    std::cout << "For service " << human << " men per month, necessary "  << necessaryBarber << " barbers." << std::endl;
+    std::cout << barber << " barber can get a haircut "
+              << barber * barberMonth  << " men in a month." << std::endl;
+
+    if (barber >= necessaryBarber){
+        std::cout << "There are enough barbers in barbershops " << std::endl;
+    } else {
+        std::cout << "There aren't enough barbers in barbershops " << std::endl;
+    }
 }
+
